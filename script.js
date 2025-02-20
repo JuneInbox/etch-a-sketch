e52357195acd2a84f333f90f07d62f3fa029b02f
JuneInbox <juneinbox@proton.me>
2025-02-20 02:05:19 +0000

    finished base javascript for the etch-a-sketch, may com,e back and make it look prettier but all functionality is here now, project is comllete

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 let input = document.getElementById('inputvalue');
 let container = document.getElementById('container');
-let value = 0;
 let div = 0;
+let number = document.getElementById('number')
 
-function createGrid() {
-    for (let i = 0; i < value * value; i++) {
-       let div = document.createElement(div)
-       div.classList.add('cells')
-       container.appendChild(div)
+function createGrid(num) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < num * num; i++) {
+        const div = document.createElement('div')
+        div.classList.add('cells')
+        container.appendChild(div)
     }
+
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+   
+    hover();
 }
+
+
+const value = document.getElementById('inputvalue')
+
+
+
+value.addEventListener('input', () => {
+    let num = value.value;
+    createGrid(num);
+ number.innerHTML = num;
+}
+);
+
+function hover() {
+    const cell = document.querySelectorAll('.cells')
+    cell.forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            const r = Math.random() * 256;
+            const g = Math.random() * 256;
+            const b = Math.random() * 256;
+            cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        });
+    });
+}    
+
 
